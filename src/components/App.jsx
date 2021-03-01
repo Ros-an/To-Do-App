42b87f815a49617f0cef4b7b105535a4bb6d0693
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-03-01 09:55:28 +0530

    added content like heading, button, input field and last for list field

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,22 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <div className="container"></div>
+      <div className="container">
+        <div className="heading">
+          <h1>To-Do</h1>
+        </div>
+        <div className="form">
+          <input type="text" />
+          <button>
+            <span> Add </span>
+          </button>
+        </div>
+        <div>
+          <ul>
+            <li>A item</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
